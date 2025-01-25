e878b7f1707be0c6ab344b4db3e5408a2bd42c9a
paolo pirruccello <gameristcanada@gmail.com>
2025-01-25 12:37:49 +0100

    add player movement

---
+++ Assets/scripts/player movement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Vector2 axis = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            axis.x = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            axis.x = 1;
+        }
+
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    axis.y = -1;
+        //}
+        //else if (Input.GetKey(KeyCode.W))
+        //{
+        //    axis.y = 1;
+        //}
+
+        body.velocity = new Vector2(axis.x * speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.velocity = new Vector2(body.velocity.x, speed);
+        }
+    }
+ }
