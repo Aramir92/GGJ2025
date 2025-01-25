2eeb49318f756bdb9928a691187dc23f37018501
paolo pirruccello <gameristcanada@gmail.com>
2025-01-25 14:02:54 +0100

    camera

---
+++ Assets/scripts/camera controller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour
+{
+[SerializeField] private float speed;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero;
+[SerializeField] private Transform player;
+[SerializeField] private float aheadDistance;
+[SerializeField] private float cameraSpeed;
+    private float lookAhead;
+    private void Update()
+    {
+        transform.position=Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed * Time.deltaTime);
+        transform.position=new Vector3(player.position.x, transform.position.y, transform.position.z);
+        lookAhead=Mathf.Lerp(lookAhead,(aheadDistance*transform.localScale.x), Time.deltaTime*cameraSpeed);
+    }
+}
